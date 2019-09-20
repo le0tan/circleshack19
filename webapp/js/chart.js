be8d22c2fe020e137f0d8025b11ba4211988e846
ZhangHuafan <e0261883@u.nus.edu>
2019-09-20 09:21:48 +0800

    Add rough layout

--- webapp/js/chart.js
+++ webapp/js/chart.js
@@ -1,3 +1,5 @@
+var data = [24, 56, 78, 32];
+var labels = ["100 ~ 200", "200 ~ 300", "300 ~ 400", "400 ~ 500"];
 var lineColor = 'rgba(75, 192, 192, 1)';
 var lineFill = 'rgba(75, 192, 192, 0.2)'
 var options = {
@@ -7,28 +9,18 @@ var options = {
     }
 }
 
-function renderChart(data, labels) {
-    var ctx = document.getElementById("mixchart").getContext('2d');
-    var chart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                data: data,
-                borderColor: lineColor,
-                backgroundColor: lineFill,
-                fill: true
-            },
-            ]
+var ctx = document.getElementById("mixchart").getContext('2d');
+var chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: labels,
+        datasets: [{
+            data: data,
+            borderColor: lineColor,
+            backgroundColor: lineFill,
+            fill: true
         },
-        options: options
-    });
-}
-
-$("#monthlyIncomeBtn").click(
-    function () {
-        data = [24, 56, 78, 32];
-        labels = ["100 ~ 200", "200 ~ 300", "300 ~ 400", "400 ~ 500"];
-        renderChart(data, labels);
-    }
-)
+        ]
+    },
+    options: options
+});
